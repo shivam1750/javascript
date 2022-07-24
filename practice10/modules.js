ea08b0bf1b22f90082ce2ba1b4426d28fbb8d8d9
Shivam borse <79648014+shivam1750@users.noreply.github.com>
2022-07-24 10:08:19 +0530

    Update modules.js

---
+++ practice10/modules.js
@@ -0,0 +1,121 @@
+// --------------Palindrome Checker --------- 
+function palindrome(str) {
+    const alphanumericOnly = str
+        // 1) Lowercase the input
+        .toLowerCase()
+        // 2) Strip out non-alphanumeric characters
+        .match(/[a-z0-9]/g);
+
+    // 3) return string === reversedString
+    return alphanumericOnly.join('') ===
+        alphanumericOnly.reverse().join('');
+}
+palindrome("eye");
+
+// -------------- Roman Numeral Converter -----------
+function convertToRoman(num) {
+    var roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var str = '';
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
+}
+
+// --------- Caesars Cipher ----------------
+function rot13(str) {
+
+    var alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', " ", "-", "_", ".", "&", "?", "!", "@", "#", "/"];
+
+    var alphabets13 = ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', " ", "-", "_", ".", "&", "?", "!", "@", "#", "/"];
+
+    var resultStr = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < alphabets.length; j++) {
+            if (str[i] === alphabets[j]) {
+                resultStr.push(alphabets13[j]);
+            }
+        }
+    }
+    return resultStr.join("");
+};
+rot13("SERR PBQR PNZC");
+
+// ---------- Telephone Number Validator ------------
+var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+function telephoneCheck(str) {
+    return re.test(str);
+}
+
+telephoneCheck("555-555-5555");
+
+// ------------- Cash Register -------------
+var denom = [
+    { name: 'ONE HUNDRED', val: 100 },
+    { name: 'TWENTY', val: 20 },
+    { name: 'TEN', val: 10 },
+    { name: 'FIVE', val: 5 },
+    { name: 'ONE', val: 1 },
+    { name: 'QUARTER', val: 0.25 },
+    { name: 'DIME', val: 0.1 },
+    { name: 'NICKEL', val: 0.05 },
+    { name: 'PENNY', val: 0.01 }
+];
+
+function checkCashRegister(price, cash, cid) {
+    var output = { status: null, change: [] };
+    var change = cash - price;
+    var register = cid.reduce(function (acc, curr) {
+        acc.total += curr[1];
+        acc[curr[0]] = curr[1];
+        return acc;
+    }, { total: 0 });
+    if (register.total === change) {
+        output.status = 'CLOSED';
+        output.change = cid;
+        return output;
+    }
+    if (register.total < change) {
+        output.status = 'INSUFFICIENT_FUNDS';
+        return output;
+    }
+    var change_arr = denom.reduce(function (acc, curr) {
+        var value = 0;
+        while (register[curr.name] > 0 && change >= curr.val) {
+            change -= curr.val;
+            register[curr.name] -= curr.val;
+            value += curr.val;
+            change = Math.round(change * 100) / 100;
+        }
+        if (value > 0) {
+            acc.push([curr.name, value]);
+        }
+        return acc;
+    }, []);
+    if (change_arr.length < 1 || change > 0) {
+        output.status = 'INSUFFICIENT_FUNDS';
+        return output;
+    }
+    output.status = 'OPEN';
+    output.change = change_arr;
+    return output;
+}
